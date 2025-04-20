871a497ca4b1388ffc2cd5d6192fd54fc7cf2dbb
ArlekinN <stepantan02@mail.ru>
2025-04-21 01:55:46 +0300

    creating a function to count pairs of characters in a file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <string.h>
 #include <stdio.h>
+#include <iostream>
+#include <map>
+#include <utility> 
+#include <fstream>
+#include <vector>
+using namespace std;
+
+map<pair<char, char>, int> pairs;
+
+void number_of_pairs(const char *filename){
+    ifstream file(filename, ios::binary);
+    if (!file.is_open()) {
+        fprintf(stderr, "Error: cannot open file '%s'\n", filename);
+        return;
+    }
+    vector<char> chars;
+    char current;
+    while (file.get(current)) {
+        chars.push_back(current);
+    }
+    if (chars.size() == 2){
+        pairs[{chars[0], chars[1]}]++;  
+        return;
+    }
+    for (size_t i = 1; i < chars.size() - 1; ++i) {
+        pairs[{chars[i - 1], chars[i]}]++;  
+        pairs[{chars[i], chars[i + 1]}]++;  
+    }
+    pairs[{chars[chars.size() - 2], chars.back()}]++;
+    file.close();
+}
 
 int main(int argc, char *argv[]){
     bool isEncoder = false;
@@ -15,6 +46,7 @@ int main(int argc, char *argv[]){
         isEncoder = true;
         input_file = argv[2];
         zip_file = argv[3];
+        number_of_pairs(input_file);
     }
     else if (strcmp(argv[1], "decoder") == 0) {
         isDecoder = true;
@@ -25,5 +57,5 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Error: first parameter must be 'encoder' or 'decoder'\n");
         return 1;
     }
-   
+    return 0;
 }
